bb4dae6931e7d9ccdd2f84b356dc373a1f47e51a
Milkias Tonji <tongisttonji@gmail.com>
2024-08-22 17:46:49 +0300

    fixed the production build errors and bugs

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,80 +1,80 @@
-import { MovieType } from '@/types'
-import img1 from '../../public/images/img1.jpeg'
-import img2 from '../../public/images/img2.jpeg'
-import img3 from '../../public/images/img3.jpeg'
+// import { MovieType } from '@/types'
+// import img1 from '../../public/images/img1.jpeg'
+// import img2 from '../../public/images/img2.jpeg'
+// import img3 from '../../public/images/img3.jpeg'
 
 
-export const data: MovieType[] = [
-    {
-        id: 1,
-        thumbnail: img1,
-        title: "Movie title 1",
-        year: 2011,
-    },
-    {
-        id: 2,
-        thumbnail: img2,
-        title: "Movie title 2",
-        year: 2011,
-    },
-    {
-        id: 3,
-        thumbnail: img3,
-        title: "Movie title 3",
-        year: 2011,
-    },
-    {
-        id: 4,
-        thumbnail: img1,
-        title: "Movie title 1",
-        year: 2011,
-    },
-    {
-        id: 5,
-        thumbnail: img2,
-        title: "Movie title 2",
-        year: 2011,
-    },
-    {
-        id: 6,
-        thumbnail: img3,
-        title: "Movie title 3",
-        year: 2011,
-    },
-    {
-        id: 7,
-        thumbnail: img1,
-        title: "Movie title 1",
-        year: 2011,
-    },
-    {
-        id: 8,
-        thumbnail: img2,
-        title: "Movie title 2",
-        year: 2011,
-    },
-    {
-        id: 9,
-        thumbnail: img3,
-        title: "Movie title 3",
-        year: 2011,
-    },
-    {
-        id: 10,
-        thumbnail: img1,
-        title: "Movie title 1",
-        year: 2011,
-    },
-    {
-        id: 11,
-        thumbnail: img2,
-        title: "Movie title 2",
-        year: 2011,
-    },
-    {
-        id: 12,
-        thumbnail: img3,
-        title: "Movie title 3",
-        year: 2011,
-    }
-]
+// export const data: MovieType[] = [
+//     {
+//         _id: 1,
+//         thumbnailUrl: img1,
+//         title: "Movie title 1",
+//         year: 2011,
+//     },
+//     {
+//         _id: 2,
+//         thumbnailUrl: img2,
+//         title: "Movie title 2",
+//         year: 2011,
+//     },
+//     {
+//         _id: 3,
+//         thumbnailUrl: img3,
+//         title: "Movie title 3",
+//         year: 2011,
+//     },
+//     {
+//         _id: 4,
+//         thumbnailUrl: img1,
+//         title: "Movie title 1",
+//         year: 2011,
+//     },
+//     {
+//         _id: 5,
+//         thumbnailUrl: img2,
+//         title: "Movie title 2",
+//         year: 2011,
+//     },
+//     {
+//         _id: 6,
+//         thumbnailUrl: img3,
+//         title: "Movie title 3",
+//         year: 2011,
+//     },
+//     {
+//         _id: 7,
+//         thumbnailUrl: img1,
+//         title: "Movie title 1",
+//         year: 2011,
+//     },
+//     {
+//         _id: 8,
+//         thumbnailUrl: img2,
+//         title: "Movie title 2",
+//         year: 2011,
+//     },
+//     {
+//         _id: 9,
+//         thumbnailUrl: img3,
+//         title: "Movie title 3",
+//         year: 2011,
+//     },
+//     {
+//         _id: 10,
+//         thumbnailUrl: img1,
+//         title: "Movie title 1",
+//         year: 2011,
+//     },
+//     {
+//         _id: 11,
+//         thumbnailUrl: img2,
+//         title: "Movie title 2",
+//         year: 2011,
+//     },
+//     {
+//         _id: 12,
+//         thumbnailUrl: img3,
+//         title: "Movie title 3",
+//         year: 2011,
+//     }
+// ]
